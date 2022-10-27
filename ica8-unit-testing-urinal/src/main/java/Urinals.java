565091e6d0a30d8aeeddbb023560322984c41035
Rhishabh Hattarki <rhattark@asu.edu>
2022-10-27 15:48:44 -0700

    basic maven project

---
+++ ica8-unit-testing-urinal/src/main/java/Urinals.java
@@ -0,0 +1,12 @@
+/**
+ * This class is used to track the urinals that are occupied.
+ * Main goal is to calculate the number of vacant urinals considering the
+ * previously occupied urinals and the unwritten urinal rule.
+ *
+ * @author Rhishabh Suhas Hattarki - rhattark
+ */
+public class Urinals {
+    public static void main(String[] args) {
+        System.out.println("basic structure");
+    }
+}
